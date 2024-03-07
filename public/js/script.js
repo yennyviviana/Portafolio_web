729c2ad628b7329c6744c5155c23c2d064d4be5e
yennyviviana <147434689+yennyviviana@users.noreply.github.com>
2024-03-07 17:49:00 -0500

    Mensaje descriptivo de los cambios

--- public/js/script.js
+++ public/js/script.js
@@ -27,3 +27,59 @@ $(document).ready(function(){
         }
     });
 });
+
+
+//Profesiones automaticamente
+
+  document.addEventListener('DOMContentLoaded', function () {
+    // Array con tus profesiones
+    var profesiones = ['Bibiana Caicedo','Analista', 'Diseñadora multimedia', 'Desarrolladora web','Desarrolladora de sofware'];
+
+    // Función para cambiar dinámicamente el nombre y la profesión
+    function cambiarNombreYProfesion() {
+        var nombreElemento = document.getElementById('nombre');
+        var profesionElemento = document.getElementById('profesion');
+
+        // Cambiar la profesión
+        var profesionActual = profesionElemento.textContent;
+        var nuevaProfesion = profesiones[(profesiones.indexOf(profesionActual) + 1) % profesiones.length];
+        profesionElemento.textContent = nuevaProfesion;
+    }
+
+    // Cambiar dinámicamente cada 2 segundos
+    setInterval(cambiarNombreYProfesion, 3000);
+
+    // Ejecutar la función una vez después de cargar la página
+    cambiarNombreYProfesion();
+});
+
+
+ /* Función para validar el formulario
+ function validateForm() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    // Validar el nombre (no debe estar vacío)
+    if (name.trim() === '') {
+        alert('Por favor, ingresa tu nombre.');
+        return false;
+    }
+
+    // Validar el correo electrónico
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert('Por favor, ingresa una dirección de correo electrónico válida.');
+        return false;
+    }
+
+    // Validar el mensaje (no debe estar vacío)
+    if (message.trim() === '') {
+        alert('Por favor, ingresa tu mensaje.');
+        return false;
+    }
+
+    // Si todas las validaciones son exitosas, permite el envío del formulario
+    return true;
+}*/
+  
